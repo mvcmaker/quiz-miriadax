73abd33e5da536a0589fe186cb7dc213899ae562
Daniel Requena Cervantes <mvcmaker@gmail.com>
2015-07-09 01:35:41 +0200

    First question

---
+++ quiz/controllers/quiz_controller.js
@@ -0,0 +1,9 @@
+exports.question = function(req, res) {
+	res.render('quizes/question', { title: 'Quiz', pregunta: 'Capital de Italia?'});
+};
+exports.answer = function(req, res) {
+	if(req.query.respuesta === 'Roma')
+		res.render('quizes/answer', { title: 'Quiz', respuesta: 'Correcto' });
+	else
+		res.render('quizes/answer', { title: 'Quiz', respuesta: 'Incorrecto' });
+};
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var quizController = require('../controllers/quiz_controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
