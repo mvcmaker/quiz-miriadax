ecfeaf40604574af6b18333d66267f94a78cedbe
Daniel Requena Cervantes <mvcmaker@gmail.com>
2015-07-14 00:53:40 +0200

    Added database Postgresql for deployment in Heroku

--- quiz/models/models.js
+++ quiz/models/models.js
@@ -1,15 +1,35 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Load ORM model
 var Sequelize = require('sequelize');
 
-// Use DB Sqlite:
-var sequelize = new Sequelize(null, null, null,
-		{ dialect:'sqlite', storage:'quiz.sqlite' }
+// Use DB Sqlite or Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+		{   dialect:dialect,
+			protocol:protocol,
+			port:port,
+			host:host,
+			storage:storage, // Only for SQLite
+			omitNull:true	// Only for Postgres
+		}
 	);
 
 // Import the definition of Quiz table in quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exports definition of Quiz table
 
